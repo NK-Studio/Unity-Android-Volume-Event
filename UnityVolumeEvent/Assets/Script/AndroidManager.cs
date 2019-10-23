f739fda43d0d007f178bd5cdebe56c4f383299bf
bnm000215 <a62201631@gmail.com>
2019-10-24 01:37:33 +0900

    간단하면서 강력하게 유니티 안드로이드 플랫폼 볼륨 키 이벤트를 지원합니다.

---
+++ UnityVolumeEvent/Assets/Script/AndroidManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AndroidManager : MonoBehaviour {
+
+    public Text CountTx;
+
+    private int Count;
+
+	void Start () {
+
+        //중요 - 오브젝트 이름을 무조건 AndroidPlugin로 해주세요.
+        gameObject.name = "AndroidPlugin";
+    }
+
+    private void Update()
+    {
+        CountTx.text = Count.ToString();
+    }
+
+    //볼륨 이벤트를 받아 옵니다.
+    public void VolumeEvent(string arg)
+    {
+        switch (arg)
+        {
+            case "Up":
+                Count += 1;
+                //원하는 내용을 넣으십시오.
+                break;
+            case "Down":
+                Count -= 1;
+                //원하는 내용을 넣으십시오.
+                break;
+        }
+    }
+}
