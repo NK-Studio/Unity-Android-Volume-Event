462873a24ad5fdf936e8349eb6d66e9612bb3f46
NK Studio <NK-Studio@users.noreply.github.com>
2024-01-28 18:25:53 +0900

    업데이트 완료

--- Assets/Editor/Android/ModifyProjectScript.cs
+++ Assets/Editor/Android/ModifyProjectScript.cs
@@ -5,8 +5,8 @@ public class ModifyProjectScript : AndroidProjectFilesModifier
 {
     public override void OnModifyAndroidProjectFiles(AndroidProjectFiles projectFiles)
     {
-        var attributesM1 = new Dictionary<string, string>() { ["android:name"] = "com.unity3d.player.UnityPlayerActivity", ["android:theme"] = "@style/UnityThemeSelector" };
+        var attributesM1 = new Dictionary<string, string>() { ["android:name"] = "com.unity3d.player.UnityPlayerGameActivity"};
         var activityM0 = projectFiles.UnityLibraryManifest.Manifest.Application.ActivityList.GetElement(attributesM1);
-        activityM0?.Attributes.Name.Set("com.unity3d.player.CustomUnityPlayerActivity");
+        activityM0?.Attributes.Name.Set("com.unity3d.player.CustomUnityPlayerGameActivity");
     }
 }
--- Assets/Plugins/Android/CustomUnityPlayerGameActivity.kt
+++ Assets/Plugins/Android/CustomUnityPlayerGameActivity.kt
@@ -1,18 +1,16 @@
 package com.unity3d.player
 
 import android.view.KeyEvent
-import com.unity3d.player.UnityPlayerActivity
-import android.widget.Toast
-import com.unity3d.player.UnityPlayer
 
+class CustomUnityPlayerGameActivity : UnityPlayerGameActivity()
+{
 
-class CustomUnityPlayerActivity : UnityPlayerActivity() {
 
     override fun onKeyDown(keyCode: Int, p1: KeyEvent?): Boolean {
 
         when (keyCode) {
             KeyEvent.KEYCODE_VOLUME_UP -> {
-             UnityPlayer.UnitySendMessage("AndroidPlugin","VolumeEvent","Up")
+                UnityPlayer.UnitySendMessage("AndroidPlugin","VolumeEvent","Up")
                 return true
             }
             KeyEvent.KEYCODE_VOLUME_DOWN -> {
@@ -20,8 +18,8 @@ class CustomUnityPlayerActivity : UnityPlayerActivity() {
                 return true
             }
         }
-
         return true
     }
+
 
 }
